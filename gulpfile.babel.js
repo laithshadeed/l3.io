bc1ef4835245b2b0d2813457e9345583d269d5f3
Laith Shadeed <laith.shadeed@gmail.com>
2016-03-23 22:46:27 +0100

    Rewrite gulp.js in ES2015

---
+++ gulpfile.babel.js
@@ -0,0 +1,78 @@
+'use strict';
+
+import del from 'del';
+import gulp from 'gulp';
+import gulploadplugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import {exec as exec} from 'child_process';
+
+gulp.task('default', ['build']);
+gulp.task('build', ['clean', 'themes']);
+gulp.task('themes', ['index', 'markdown', 'ace', 'so', 'ln']);
+gulp.task('clean', () => del(['*.html', 'resume.md', '*.pdf', 'dist']));
+
+const $ = gulploadplugins();
+const resume = './node_modules/.bin/resume';
+const theme = './node_modules/jsonresume-theme';
+const tasks = [
+  {
+    name: 'index',
+    cmd: `${resume} export --theme ${theme}-business-card-minimal index.html`
+  },
+  {
+    name: 'markdown',
+    cmd: `${resume} export --theme ${theme}-markdown resume.md`
+  },
+  {
+    name: 'ace',
+    cmd: `${resume} export --theme ${theme}-ace resume-ace.html`
+  },
+  {
+    name: 'so',
+    cmd: `${resume} export --theme ${theme}-stackoverflow resume-so.html`
+  },
+  {
+    name: 'print',
+    cmd: `${resume} export --theme ${theme}-sshorter resume-print.html`
+  }
+];
+
+tasks.forEach(task => {
+  gulp.task(task.name, cb => {
+    exec(task.cmd, cb);
+  });
+});
+
+gulp.task('pdf', ['print'], cb => {
+  exec('./node_modules/.bin/html-pdf resume-print.html resume.pdf', cb);
+});
+
+gulp.task('ln', ['pdf'], () => {
+  exec('ln -sf resume.pdf r.pdf');
+});
+
+gulp.task('serve', ['build'], () => {
+  browserSync({
+    logPrefix: 'l3.io',
+    server: ['.'],
+    port: 3000
+  });
+});
+
+gulp.task('dist', ['build'], () => {
+  gulp.src(['*.{html,jpg,md,json,pdf}']).pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint', () => {
+  gulp.src('gulpfile.babel.js')
+    .pipe($.eslint())
+    .pipe($.eslint.format());
+});
+
+gulp.task('serve:dist', ['dist'], () => {
+  browserSync({
+    logPrefix: 'l3.io',
+    server: ['dist'],
+    port: 3000
+  });
+});
